5d21d27245ee586b5561b7f158b6b3264831d637
Nimscore <gamenimsi@gmail.com>
2022-12-16 22:46:55 +0300

    up

--- js/monk/gameplaymw.js
+++ js/monk/gameplaymw.js
@@ -16,12 +16,14 @@ $(document).ready(function() {
 	});
 
 	$(".rb2").click(function(){ //Освежающий нефритовый ветер
-		if ($(".rp10").css('display') == 'none') {
+		if ($(".srp8").css('display') == 'none') {
+			$(".srp8").fadeIn(600); //рейд
 
 
 			$(".rb2").removeClass("fa-xmark");
 			$(".rb2").addClass("fa-check");
 		} else {
+			$(".srp8").fadeOut(200);
 
 
 			$(".rb2").removeClass("fa-check");
@@ -32,11 +34,20 @@ $(document).ready(function() {
 	$(".rb3").click(function(){ //Волшебная линия
 		if ($(".asrp3").css('display') == 'none') {
 			$(".asrp3").fadeIn(600); //мифик+
+			$(".srp1").fadeIn(600); //рейд
+			$(".lsrp2").fadeIn(600); //рейд
+			$(".dsrp5").fadeIn(600); //урон
+			$(".dsrp6").fadeIn(600); //урон
 
 			$(".rb3").removeClass("fa-xmark");
 			$(".rb3").addClass("fa-check");
 		} else {
 			$(".asrp3").fadeOut(200);
+			$(".srp1").fadeOut(200);
+			$(".srp13").fadeOut(200);
+			$(".lsrp2").fadeOut(200);
+			$(".dsrp5").fadeOut(200);
+			$(".dsrp6").fadeOut(200);
 
 			$(".rb3").removeClass("fa-check");
 			$(".rb3").addClass("fa-xmark");
@@ -46,25 +57,47 @@ $(document).ready(function() {
 	$(".rb4").click(function(){ //Дзен-импульс
 		if ($(".asrp2").css('display') == 'none') {
 			$(".asrp2").fadeIn(600); //мифик+
+			$(".dsrp2").fadeIn(600); //урон
 
 			$(".rb4").removeClass("fa-xmark");
 			$(".rb4").addClass("fa-check");
 		} else {
 			$(".asrp2").fadeOut(200);
+			$(".dsrp2").fadeOut(200);
 
 			$(".rb4").removeClass("fa-check");
 			$(".rb4").addClass("fa-xmark");
 		}
 	});
 
-	$(".rb9").click(function(){ //Подъем азерита
-		if ($(".srp15").css('display') == 'none') {
+	$(".rb5").click(function(){ //Призыв статуи белого тигра
+		if ($(".dsrp1").css('display') == 'none') {
+			$(".dsrp1").fadeIn(600); //урон
 
+			$(".rb5").removeClass("fa-xmark");
+			$(".rb5").addClass("fa-check");
+		} else {
+			$(".dsrp1").fadeOut(200);
+
+			$(".rb5").removeClass("fa-check");
+			$(".rb5").addClass("fa-xmark");
+		}
+	});
+
+	$(".rb9").click(function(){ //Подъем азерита
+		if ($(".srp5").css('display') == 'none') {
+			$(".srp6").fadeOut(200);
+			$(".srp5").fadeIn(600); //рейд
+			$(".srp7").fadeOut(200);
+			$(".srp16").fadeIn(600); //рейд
 
 			$(".rb9").removeClass("fa-xmark");
 			$(".rb9").addClass("fa-check");
 		} else {
-
+			$(".srp5").fadeOut(200);
+			$(".srp6").fadeIn(600);
+			$(".srp16").fadeOut(200);
+			$(".srp7").fadeIn(600);
 
 			$(".rb9").removeClass("fa-check");
 			$(".rb9").addClass("fa-xmark");
@@ -88,11 +121,15 @@ $(document).ready(function() {
 	$(".rb11").click(function(){ //Древние учения
 		if ($(".asrp6").css('display') == 'none') {
 			$(".asrp6").fadeIn(600); //мифик+
+			$(".lsrp6").fadeOut(200); //рейд
+			$(".lsrp5").fadeIn(600); //рейд
 
 			$(".rb11").removeClass("fa-xmark");
 			$(".rb11").addClass("fa-check");
 		} else {
 			$(".asrp6").fadeOut(200);
+			$(".lsrp5").fadeOut(200); //рейд
+			$(".lsrp6").fadeIn(600); //рейд
 
 			$(".rb11").removeClass("fa-check");
 			$(".rb11").addClass("fa-xmark");
@@ -100,27 +137,36 @@ $(document).ready(function() {
 	});
 
 	$(".rb12").click(function(){ //Затуманенная концентрация
-		if ($(".srp11").css('display') == 'none') {
+		if ($(".srp2").css('display') == 'none') {
+			$(".srp2").fadeIn(600); //рейд
+			$(".srp4").fadeIn(600); //рейд
+			$(".srp10").fadeIn(600); //рейд
+			$(".srp12").fadeIn(600); //рейд
 
 
 			$(".rb12").removeClass("fa-xmark");
 			$(".rb12").addClass("fa-check");
 		} else {
-
+			$(".srp2").fadeOut(200);
+			$(".srp4").fadeOut(200);
+			$(".srp10").fadeOut(200);
+			$(".srp12").fadeOut(200);
 
 			$(".rb12").removeClass("fa-check");
 			$(".rb12").addClass("fa-xmark");
 		}
 	});
 
-	$(".rb13").click(function(){ //Живенькое оживление
-		if ($(".srp11").css('display') == 'none') {
-
+	$(".rb13").click(function(){ //Древнее согласие
+		if ($(".dsrp3").css('display') == 'none') {
+			$(".dsrp3").fadeIn(600); //урон
+			$(".dsrp4").fadeIn(600); //урон
 
 			$(".rb13").removeClass("fa-xmark");
 			$(".rb13").addClass("fa-check");
 		} else {
-
+			$(".dsrp3").fadeOut(200);
+			$(".dsrp4").fadeOut(200);
 
 			$(".rb13").removeClass("fa-check");
 			$(".rb13").addClass("fa-xmark");
@@ -131,12 +177,20 @@ $(document).ready(function() {
 		if ($(".asrp10").css('display') == 'none') {
 			$(".asrp7").fadeOut(200);
 			$(".asrp10").fadeIn(600); //мифик+
+			$(".srp6").fadeOut(200);
+			$(".srp5").fadeIn(600); //рейд
+			$(".srp7").fadeOut(200);
+			$(".srp16").fadeIn(600); //рейд
 
 			$(".rb14").removeClass("fa-xmark");
 			$(".rb14").addClass("fa-check");
 		} else {
 			$(".asrp10").fadeOut(200);
 			$(".asrp7").fadeIn(600);
+			$(".srp5").fadeOut(200);
+			$(".srp6").fadeIn(600);
+			$(".srp16").fadeOut(200);
+			$(".srp7").fadeIn(600);
 
 			$(".rb14").removeClass("fa-check");
 			$(".rb14").addClass("fa-xmark");
